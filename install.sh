fa565c9d2d34f9a1aa6569b3aae594d70733b11d
WingLim <643089849@qq.com>
2020-12-04 01:42:53 +0800

    feat: add if installed detection

--- install.sh
+++ install.sh
@@ -45,14 +45,14 @@ set_system() {
 }
 
 clone_repo() {
-    hint "* Cloning WingLim/dotfiles"
+    ok "* Cloning WingLim/dotfiles"
     
     git clone https://github.com/WingLim/dotfiles "$HOME/dotfiles"
     rm -rf "$HOME/dotfiles/.git"
 }
 
 install_package() {
-    hint "* Installing packages"
+    ok "* Installing packages"
     __pkg_to_be_installed=(
             zsh
             wget
@@ -83,80 +83,94 @@ install_package() {
 }
 
 setup_omz() {
-    hint "* Installing Oh-My-Zsh"
-    curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
-    hint "* Installing ZSH Plugins & Themes:"
-    hint "  - zsh-autosuggestions"
-    hint "  - zsh-completions"
-    hint "  - zsh-z"
-    hint "  - fast-syntax-highlighting"
-    hint "  - powerlevel10k zsh-theme"
+    ok "* Installing Oh-My-Zsh"
+    if ! [ -d "$HOME/.oh-my-zsh" ]; then
+        curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+        ok "* Installing ZSH Plugins & Themes:"
+        ok "  - zsh-autosuggestions"
+        ok "  - zsh-completions"
+        ok "  - zsh-z"
+        ok "  - fast-syntax-highlighting"
+        ok "  - powerlevel10k zsh-theme"
 
 
-    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-    git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions"
-    git clone https://github.com/agkozak/zsh-z "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-z"
-    git clone https://github.com/zdharma/fast-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
-    
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+        git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+        git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions"
+        git clone https://github.com/agkozak/zsh-z "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-z"
+        git clone https://github.com/zdharma/fast-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
+        
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    else
+        warn "! Oh-My-Zsh alread installed"
+    fi
 }
 
 install_pyenv() {
-    hint "* Installing pyenv"
-    local GITHUB="https://github.com"
-    local PYENV_ROOT="$HOME/.pyenv"
-    git clone "${GITHUB}/pyenv/pyenv.git"            "${PYENV_ROOT}"
-    git clone "${GITHUB}/pyenv/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"
-    git clone "${GITHUB}/pyenv/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer"
-    git clone "${GITHUB}/pyenv/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"
-    git clone "${GITHUB}/pyenv/pyenv-virtualenv.git" "${PYENV_ROOT}/plugins/pyenv-virtualenv"
-    git clone "${GITHUB}/pyenv/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext"
-    export PYENV_ROOT=$PYENV_ROOT
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-}
-
-install_python() {
-    hint "* Installing python 3.9.0"
-    pyenv -v
-    pyenv install 3.9.0
-    pyenv global 3.9.0
+    ok "* Installing pyenv"
+    if ! [ -d "$HOME/.pyenv" ]; then
+        local GITHUB="https://github.com"
+        local PYENV_ROOT="$HOME/.pyenv"
+        git clone "${GITHUB}/pyenv/pyenv.git"            "${PYENV_ROOT}"
+        git clone "${GITHUB}/pyenv/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"
+        git clone "${GITHUB}/pyenv/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer"
+        git clone "${GITHUB}/pyenv/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"
+        git clone "${GITHUB}/pyenv/pyenv-virtualenv.git" "${PYENV_ROOT}/plugins/pyenv-virtualenv"
+        git clone "${GITHUB}/pyenv/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext"
+        export PYENV_ROOT=$PYENV_ROOT
+        export PATH="$PYENV_ROOT/bin:$PATH"
+        eval "$(pyenv init -)"
+        eval "$(pyenv virtualenv-init -)"
+        
+        ok "* Installing python 3.9.0"
+        pyenv -v
+        pyenv install 3.9.0
+        pyenv global 3.9.0
+    else
+        warn "! Pyenv alread installed"
+    fi
 }
 
 install_goenv() {
-    hint "* Installing syndbg/goenv"
+    ok "* Installing syndbg/goenv"
+    if ! [ -d "$HOME/.goenv" ]; then
+        git clone https://github.com/syndbg/goenv.git "$HOME/.goenv"
 
-    git clone https://github.com/syndbg/goenv.git "$HOME/.goenv"
+        export GOENV_ROOT="$HOME/.goenv"
+        export PATH="$GOENV_ROOT/bin:$PATH"
+        eval "$(goenv init -)"
 
-    export GOENV_ROOT="$HOME/.goenv"
-    export PATH="$GOENV_ROOT/bin:$PATH"
-    eval "$(goenv init -)"
-
-    hint "* Installing golang 1.15.5"
-    goenv install 1.15.5
-    goenv global 1.15.5
+        ok "* Installing golang 1.15.5"
+        goenv install 1.15.5
+        goenv global 1.15.5
+    else
+        warn "! Goenv alread installed"
+    fi
 }
 
 install_thinkvim() {
-    mkdir -p "$HOME/.npm-global"
-    export NPM_CONFIG_PREFIX="$HOME/.npm-global"
-    export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
-    git clone --depth=1 https://github.com/hardcoreplayers/ThinkVim.git ~/.config/nvim
-    npm install -g yarn
-    pyenv virtualenv 3.9.0 neovim
-    mkdir -p "$HOME/.thinkvim.d"
-    cat "$HOME/dotfiles/thinkvim/plugins.yaml" > "$HOME/.thinkvim.d/plugins.yaml"
-    cd ~/.config/nvim || exit
-    # this install script will install bat and ripgrep, may need to confirm
-    echo y | bash scripts/install.sh
-    yarn global add \
-        dockerfile-language-server-nodejs \
-        bash-language-server intelephense
+    ok "* Installing ThinkVim"
+    if ! [ -d "$HOME/.thinkvim.d" ]; then
+        mkdir -p "$HOME/.npm-global"
+        export NPM_CONFIG_PREFIX="$HOME/.npm-global"
+        export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
+        git clone --depth=1 https://github.com/hardcoreplayers/ThinkVim.git ~/.config/nvim
+        npm install -g yarn
+        pyenv virtualenv 3.9.0 neovim
+        mkdir -p "$HOME/.thinkvim.d"
+        cat "$HOME/dotfiles/thinkvim/plugins.yaml" > "$HOME/.thinkvim.d/plugins.yaml"
+        cd ~/.config/nvim || exit
+        # this install script will install bat and ripgrep, may need to confirm
+        echo y | bash scripts/install.sh
+        yarn global add \
+            dockerfile-language-server-nodejs \
+            bash-language-server intelephense
+    else
+        warn "! ThinkVim alread installed"
+    fi
 }
 
 install_ccls() {
-    hint "* Installing ccls"
+    ok "* Installing ccls"
     if ! [ -x "$(command -v ccls)" ]; then
         # if $cdn is 1, download pre-built binaries from tsinghua mirror
         if [ "$1" == 1 ]; then
@@ -173,12 +187,14 @@ install_ccls() {
         cd ccls || exit
         cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$HOME/src/clang+llvm-11.0.0"
         sudo cmake --build Release --target install
+    else
+        warn "! CCLS alread installed"
     fi
 }
 
 # add function for shell enable clash proxy
 clash_proxy() {
-    hint "* Setting clash proxy"
+    ok "* Setting clash proxy"
     if [[ $(uname -r) =~ "microsoft" ]]
     then
         local ip="\$(ip route | grep default | awk '{print \$3}')"
@@ -202,20 +218,24 @@ EOF
 }
 
 zshrc() {
-    hint "* Import .zshrc"
+    ok "* Import .zshrc"
     cat "$HOME/dotfiles/_zshrc/.zshrc" > "$HOME/.zshrc"
     cat "$HOME/dotfiles/p10k/.p10k.zsh" > "$HOME/.p10k.zsh"
 }
 
 finish() {
-    hint "* Clean up..."
+    ok "* Clean up..."
     rm -rf "$HOME/dotfiles"
-    hint "* Please use below command to set default shell"
-    hint "chsh -s /bin/zsh"
+    ok "* Please use below command to set default shell"
+    ok "chsh -s /bin/zsh"
 }
 
-hint() {
+ok() {
     echo -e "\033[32m$1\033[0m"
+}
+
+warn() {
+    echo -e "\033[33m$1\033[0m"
 }
 
 ARGS=$(getopt --long novim,cdn,noproxy -- "$@")
@@ -248,7 +268,6 @@ install_package
 clone_repo
 setup_omz
 install_pyenv
-install_python
 install_goenv
 zshrc
 if ! [ "$novim" == 1 ];then

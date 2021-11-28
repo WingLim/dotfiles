6b895a91cc8892f03f31f63b2ddaaa653fdcfd39
WingLim <643089849@qq.com>
2021-11-28 17:39:26 +0800

    feat: use starship

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# check system and set installer
+# Check system and set installer
 check_system() {
     if [ "$(uname)" == "Darwin" ]; then
         if ! [ -x "$(command -v brew)" ]; then
@@ -18,7 +18,7 @@ check_system() {
     fi
 }
 
-# set node package name and update software
+# Set node package name and update software
 set_system() {
     case "$OS" in
         "Darwin")
@@ -57,8 +57,9 @@ install_package() {
             cmake
             gcc
 	        ruby
+            lua
         )
-    # if not mac, install below lib for compile python
+    # If not mac, install below lib for compile python
     if ! [ "$OS" == "Darwin" ];then
         __pkg_to_be_installed+=(
 	        # for compile ccls
@@ -78,41 +79,31 @@ install_package() {
     done
 }
 
-setup_omz() {
-    ok "* Installing Oh-My-Zsh"
-    if ! [ -d "$HOME/.oh-my-zsh" ]; then
-        curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
-        ok "* Installing ZSH Plugins & Themes:"
-        ok "  - zsh-autosuggestions"
-        ok "  - zsh-completions"
-        ok "  - zsh-z"
-        ok "  - fast-syntax-highlighting"
-        ok "  - powerlevel10k zsh-theme"
+install_starship() {
+    ok "* Installing Starship"
+    mkdir -p "$HOME/.starship/bin"
+    sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y -b $HOME/.starship/bin
+}
 
+install_zsh_plugins() {
+    ok "* Installing ZSH Plugins"
+    ok "  - zsh-autosuggestions"
+    ok "  - zsh-completions"
+    ok "  - fast-syntax-highlighting"
 
-        git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-        git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions"
-        git clone https://github.com/agkozak/zsh-z "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-z"
-        git clone https://github.com/zdharma/fast-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
-        
-        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-    else
-        warn "! Oh-My-Zsh already installed"
-    fi
+    mkdir -p "$HOME/.zsh/plugins"
+
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh/plugins/zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-completions "$HOME/.zsh/plugins/zsh-completions"
+    git clone https://github.com/zdharma/fast-syntax-highlighting.git "$HOME/.zsh/plugins/fast-syntax-highlighting"
+    git clone https://github.com/skywind3000/z.lua.git "$HOME/.zsh/plugins/z"
 }
 
 install_pyenv() {
     ok "* Installing pyenv"
     if ! [ -d "$HOME/.pyenv" ]; then
-        local GITHUB="https://github.com"
-        local PYENV_ROOT="$HOME/.pyenv"
-        git clone "${GITHUB}/pyenv/pyenv.git"            "${PYENV_ROOT}"
-        git clone "${GITHUB}/pyenv/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"
-        git clone "${GITHUB}/pyenv/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer"
-        git clone "${GITHUB}/pyenv/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"
-        git clone "${GITHUB}/pyenv/pyenv-virtualenv.git" "${PYENV_ROOT}/plugins/pyenv-virtualenv"
-        git clone "${GITHUB}/pyenv/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext"
-        export PYENV_ROOT=$PYENV_ROOT
+        curl https://pyenv.run | bash
+        export PYENV_ROOT=$HOME/.pyenv
         export PATH="$PYENV_ROOT/bin:$PATH"
         eval "$(pyenv init -)"
         eval "$(pyenv virtualenv-init -)"
@@ -124,13 +115,6 @@ install_pyenv() {
     else
         warn "! Pyenv already installed"
     fi
-}
-
-install_go() {
-    ok "* Installing go"
-    wget -q -O - https://git.io/vQhTU | bash
-    source ~/.bashrc
-    go version
 }
 
 install_deno() {
@@ -155,7 +139,7 @@ install_spacevim() {
     fi
 }
 
-# add function for shell enable clash proxy
+# Add function for shell enable clash proxy
 clash_proxy() {
     ok "* Setting clash proxy"
     if [[ $(uname -r) =~ "microsoft" ]]
@@ -183,13 +167,12 @@ noproxy() {
 EOF
 }
 
-zshrc() {
+import_zshrc() {
     ok "* Import .zshrc"
     cat "$HOME/dotfiles/_zshrc/.zshrc" > "$HOME/.zshrc"
-    cat "$HOME/dotfiles/p10k/.p10k.zsh" > "$HOME/.p10k.zsh"
 }
 
-finish() {
+cleanup() {
     ok "* Clean up..."
     rm -rf "$HOME/dotfiles"
     ok "* Please use below command to set default shell"
@@ -227,15 +210,15 @@ done
 check_system
 set_system
 install_package
-clone_repo
-setup_omz
+install_starship
+install_zsh_plugins
 install_pyenv
-install_go
 install_deno
 install_pnpm_node
 install_spacevim
-zshrc
+clone_repo
+import_zshrc
 if ! [ "$noproxy" == 1 ]; then
     clash_proxy
 fi
-finish
+cleanup
